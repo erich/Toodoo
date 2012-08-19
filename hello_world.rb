92d53dcb0b43f38c756ff84b4507d5832e77ef81
Erich Kaderka <erich.kaderka@gmail.com>
2012-08-19 14:06:36 +0100

    Added tests and implementation for get, post and edit tasks

--- hello_world.rb
+++ hello_world.rb
@@ -4,12 +4,23 @@ Cuba.use Rack::Session::Cookie
 
 Cuba.define do
 	on get do
-		on "hello" do
-			res.write "Hello world!"
-		end
 
 		on root do
-			res.redirect "/hello"
+			res.redirect "/tasks"
+		end
+
+		on "tasks" do
+			res.write "List of tasks <br/> Add new task"
+		end
+
+		on  "task/:id/edit" do
+			res.write "Edit task"
+		end
+	end
+
+	on post do
+		on "tasks" do
+			res.redirect "/tasks"
 		end
 	end
 end
--- test/hello_world_test.rb
+++ test/hello_world_test.rb
@@ -1,10 +1,44 @@
 require_relative './../hello_world.rb'
 require "cuba/test"
 
+#Reopen Cutest class to add assert_match
+class Cutest::Scope
+	private
+	#last_response.body returns string, don't want to write exact whole string to make test past
+	def assert_match(value, other)
+		flunk("#{value.inspect} =~ #{other.inspect}") unless other =~ Regexp.new(value)
+		success
+	end
+end
+
 scope do
+
+	setup do 
+		@valid_task = ""
+		@invalid_task = ""
+	end
+
   test "Homepage" do
     get "/"
     follow_redirect!
-    assert_equal "Hello world!", last_response.body
+    assert_match  "List of tasks", last_response.body
   end
+
+
+	test "Adding new valid task" do
+		post "/tasks"
+		follow_redirect!
+		assert_match "Task was added successfully", last_response.body
+	end
+
+	test "Adding new invalid task" do
+		post "/tasks"
+		follow_redirect!
+		assert_match "Task is not valid", last_response.body
+	end
+
+	test "Edit new task" do
+		get "/tasks/1" 
+		assert_match "New task", last_response
+	end
 end
